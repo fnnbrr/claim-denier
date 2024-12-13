3fdd124822d461610c4b0e21ee999085507e1689
fnnbrr <9000395+fnnbrr@users.noreply.github.com>
2024-12-13 14:57:35 -0500

    First pass on score load/save

--- src/lib/inventory.svelte.ts
+++ src/lib/inventory.svelte.ts
@@ -1,3 +1,5 @@
+const saveKey: string = "claim-denier-inventory-save";
+
 export class Inventory
 {
     static get instance()
@@ -11,7 +13,45 @@ export class Inventory
     {
         console.log("Initializing Inventory");
         this.#instance = new Inventory();
+
+        if (typeof localStorage !== "undefined")
+        {
+            this.load();
+
+            addEventListener("beforeunload", () => this.save());
+        }
+    }
+
+    static load()
+    {
+        const saveString = localStorage.getItem(saveKey);
+
+        if (saveString !== null)
+        {
+            console.log("Loading save");
+
+            const save: InventorySaveData = JSON.parse(saveString);
+
+            this.instance.score = save.score;
+        }
+    }
+
+    static save()
+    {
+        console.log("saving");
+
+        const save: InventorySaveData =
+        {
+            score: this.instance.score
+        };
+
+        localStorage.setItem(saveKey, JSON.stringify(save));
     }
 
     score: number = $state(0);
+}
+
+interface InventorySaveData
+{
+    score: number;
 }

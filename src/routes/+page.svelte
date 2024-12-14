e8b760c3a51e4e17e7c3cd9faf9b86844d8a254e
fnnbrr <9000395+fnnbrr@users.noreply.github.com>
2024-12-14 09:25:43 -0500

    Page title now updates to match score, added favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
+  import { Inventory } from "$lib/inventory.svelte";
   import "../app.css";
   import GameView from "../game-view.svelte";
   import Store from "../store.svelte";
+
+  let title: string = $derived.by(() => {
+    if (Inventory.instance.score > 0) {
+      return `${Inventory.instance.score} Claims Denied`;
+    } else {
+      return "Claim Denier";
+    }
+  });
 </script>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
 
 <div class="row">
   <GameView></GameView>

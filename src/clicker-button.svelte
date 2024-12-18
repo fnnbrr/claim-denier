e9c9d8b5e8e06ff13a282152db6e013d180695e9
fnnbrr <9000395+fnnbrr@users.noreply.github.com>
2024-12-18 18:50:17 -0500

    Added scale spring and sfx to button press

--- src/clicker-button.svelte
+++ src/clicker-button.svelte
@@ -1,18 +1,35 @@
 <script lang="ts">
   import { Inventory } from "$lib/inventory.svelte";
+  import { Spring } from "svelte/motion";
+  import clickSfx from "$lib/assets/audio/error_005.ogg";
 
-  function onclick() {
+  const spring: Spring<number> = new Spring(1);
+  let audio: HTMLAudioElement;
+
+  function onpointerdown() {
     Inventory.instance.onClick();
+    spring.target = spring.current + 0.5;
+
+    setTimeout(() => {
+      spring.target = 1;
+    }, 50);
+
+    audio.currentTime = 0;
+    audio.play();
   }
 
   function oncontextmenu(e: MouseEvent) {
     e.preventDefault();
-    onclick();
   }
 </script>
 
-<button {onclick} {oncontextmenu}>
+<button
+  {onpointerdown}
+  {oncontextmenu}
+  style="transform: scale({spring.current})"
+>
   DENY
   <br />
   CLAIM
+  <audio src={clickSfx} volume={0.5} bind:this={audio}></audio>
 </button>
